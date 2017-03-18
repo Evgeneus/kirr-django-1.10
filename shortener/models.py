7ed90c66e8101270a064809c58de95ca402850e2
Evgeny <krivosheevevgeny@gmail.com>
2017-03-18 17:24:41 +0100

    feature/utils.py & save model method

--- shortener/models.py
+++ shortener/models.py
@@ -2,11 +2,20 @@ from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
+from .utils import create_shortcode
 
 
 @python_2_unicode_compatible
 class KirrURL(models.Model):
-    url = models.CharField(max_length=220, )
+    url = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.shortcode is None or self.shortcode == '':
+            self.shortcode = create_shortcode(self)
+        super(KirrURL, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
